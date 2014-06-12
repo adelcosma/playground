cfbfb1ca9eb08acdea8e7e6161e0cb943d41dde9
adelcosma <adelcosma@yahoo.com>
2014-06-12 20:34:15 +0200

    XSS bug

--- assets/js/books.js
+++ assets/js/books.js
@@ -1,8 +1,17 @@
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
+
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    l = location.search.replace(/[%3C]/,"");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        results = regex.exec(l);
+    if (results == null || !validateEmail(results[1])) {
+    	return "";
+    }
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 var email = getParameterByName('email');
